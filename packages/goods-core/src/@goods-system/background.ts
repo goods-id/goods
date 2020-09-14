e43f1de7716ae03e9fe0a50afc2f81057659364e
wiryar <57610971+wiryar@users.noreply.github.com>
2020-09-14 17:25:59 +0700

    @goods-system/background-position and husky commitlint (#86)

---
+++ packages/goods-core/src/@goods-system/background.ts
@@ -0,0 +1,30 @@
+import * as CSS from 'csstype'
+import { system, ResponsiveValue } from 'styled-system'
+
+import { ThemeType } from '../theme'
+import { Config } from '../@types/global'
+
+export interface BackgroundProps<
+  Theme extends ThemeType = ThemeType,
+  TLength = string | number
+> {
+  bgs?: ResponsiveValue<CSS.BackgroundProperty<TLength>, Theme>
+  bgImage?: ResponsiveValue<CSS.BackgroundImageProperty, Theme>
+  bgSize?: ResponsiveValue<CSS.BackgroundSizeProperty<TLength>, Theme>
+  bgPosi?: ResponsiveValue<CSS.BackgroundPositionProperty<TLength>, Theme>
+  bgRepeat?: ResponsiveValue<CSS.BackgroundRepeatProperty, Theme>
+  bgClip?: ResponsiveValue<CSS.BackgroundClipProperty, Theme>
+  bgAttach: ResponsiveValue<CSS.BackgroundAttachmentProperty, Theme>
+}
+
+const config: Config<BackgroundProps> = {
+  bgs: { property: 'background' },
+  bgImage: { property: 'backgroundImage' },
+  bgSize: { property: 'backgroundSize' },
+  bgPosi: { property: 'backgroundPosition' },
+  bgRepeat: { property: 'backgroundRepeat' },
+  bgClip: { property: 'backgroundClip' },
+  bgAttach: { property: 'backgroundAttachment' },
+}
+
+export const background = system(config)
---
+++ packages/goods-core/src/@goods-system/position.ts
@@ -0,0 +1,32 @@
+import * as CSS from 'csstype'
+import { system, get, ResponsiveValue } from 'styled-system'
+
+import { ThemeType } from '../theme'
+import { Config } from '../@types/global'
+
+export interface PositionProps<
+  Theme extends ThemeType = ThemeType,
+  TLength = string | number
+> {
+  posi?: ResponsiveValue<CSS.PositionProperty, Theme>
+  z?: ResponsiveValue<CSS.ZIndexProperty, Theme>
+  top?: ResponsiveValue<CSS.TopProperty<TLength>, Theme>
+  right?: ResponsiveValue<CSS.RightProperty<TLength>, Theme>
+  bottom?: ResponsiveValue<CSS.BottomProperty<TLength>, Theme>
+  left?: ResponsiveValue<CSS.LeftProperty<TLength>, Theme>
+}
+
+const isNumber = n => typeof n === 'number' && !Number.isNaN(n)
+const getPosition = (n, scale) =>
+  get(scale, n, !isNumber(n) || n > 1 ? n : `${n * 100}%`)
+
+const config: Config<PositionProps> = {
+  posi: { property: 'position' },
+  z: { property: 'zIndex' },
+  top: { property: 'top', transform: getPosition },
+  right: { property: 'right', transform: getPosition },
+  bottom: { property: 'bottom', transform: getPosition },
+  left: { property: 'left', transform: getPosition },
+}
+
+export const position = system(config)
